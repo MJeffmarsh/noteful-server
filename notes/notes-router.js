3f3e9e8348af2b02a45d403d1024f2c493ddddc0
Jeffrey Marshall <m.jeffrey.marshall@gmail.com>
2020-01-23 16:06:13 +0000

    possibly fixing migrations

---
+++ notes/notes-router.js
@@ -0,0 +1,94 @@
+const path = require('path');
+const express = require('express');
+const xss = require('xss');
+const NotesService = require('./notes-service');
+
+const notesRouter = express.Router();
+const jsonParser = express.json();
+
+function serializeNote(note) {
+  return {
+    id: note.id,
+    title: xss(note.title), // sanitize title
+    content: xss(note.content), // sanitize content
+    date_modified: note.date_modified,
+    folder: note.folder
+  };
+}
+
+notesRouter
+  .route('/')
+  .get((req, res, next) => {
+    NotesService.getAllNotes(req.app.get('db'))
+      .then(notes => {
+        res.json(notes.map(serializeNote));
+      })
+      .catch(next);
+  })
+  .post(jsonParser, (req, res, next) => {
+    const { title, content, folder } = req.body;
+    const newNote = { title, content };
+
+    for (const [key, value] of Object.entries(newNote)) {
+      if (value == null) {
+        return res.status(400).json({
+          error: { message: `Missing '${key}' in request body` }
+        });
+      }
+    }
+
+    newNote.folder = folder;
+    NotesService.insertNote(req.app.get('db'), newNote)
+      .then(note => {
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `/${note.id}`))
+          .json(serializeNote(note));
+      })
+      .catch(next);
+  });
+
+notesRouter
+  .route('/:note_id')
+  .all((req, res, next) => {
+    NotesService.getById(req.app.get('db'), req.params.note_id)
+      .then(note => {
+        if (!note) {
+          return res.status(404).json({
+            error: { message: `Note doesn't exist` }
+          });
+        }
+        res.note = note; // save the note for the next middleware
+        next(); // don't forget to call next so the next middleware happens!
+      })
+      .catch(next);
+  })
+  .get((req, res, next) => {
+    res.json(serializeNote(res.note));
+  })
+  .delete((req, res, next) => {
+    NotesService.deleteNote(req.app.get('db'), req.params.note_id)
+      .then(() => {
+        res.status(204).end();
+      })
+      .catch(next);
+  })
+  .patch(jsonParser, (req, res, next) => {
+    const { title, content } = req.body;
+    const noteToUpdate = { title, content };
+
+    const numberOfValues = Object.values(noteToUpdate).filter(Boolean).length;
+    if (numberOfValues === 0) {
+      return res.status(400).json({
+        error: {
+          message: `Request body must contain either 'title' or 'content'`
+        }
+      });
+    }
+
+    NotesService.updateNote(req.app.get('db'), req.params.note_id, noteToUpdate)
+      .then(numRowsAffected => {
+        res.status(204).end();
+      })
+      .catch(next);
+  });
